65048cf97f76558ab5cd7921db9d98983a7738b4
JY <76254578+chojaeyoon@users.noreply.github.com>
2021-01-13 13:17:00 +0900

    Delete scott directory

--- scott/src/test.js
+++
@@ -1,8 +0,0 @@
-const Api = require("./index");
-
-(async () => {
-  const event = {
-    queryStringParameters: { inputUrl: "https://place.map.kakao.com/8460978" },
-  };
-  Api.getInfo(event);
-})();
